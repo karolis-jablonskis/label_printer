8e4cc1df2ec2452bd7d0e5c25aacf398863d94cd
karolis-jablonskis <66665581+karolis-jablonskis@users.noreply.github.com>
2025-05-16 06:49:28 +0300

    Delete build.bat

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Building your EXE...
-
-pyinstaller main.py --hidden-import=win32print --hidden-import=win32api --onefile --noconsole --name=LabelPrinter --icon=logo.ico --additional-hooks-dir=.
-
-echo.
-echo Build complete!
-pause
